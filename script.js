53d364ad7b0c956ab0e7dc864c0b14cc88f9a4e2
Rod <dellierarodrigo@gmail.com>
2022-12-13 02:08:24 -0300

    Menu hamburguesa, traductor en mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Codigo para cambiar de idoma - Code to change the languaje
 
-var check = document.querySelector(".check")
+let check = document.querySelector(".check")
 check.addEventListener("click", idioma)
 
 function idioma(){
@@ -13,3 +13,35 @@ function idioma(){
     }
 }
 
+// change languaje Mobile.
+
+let checkMobile = document.querySelector(".check-mobile")
+checkMobile.addEventListener("click", idiomaM)
+
+function idiomaM(){
+    let id = checkMobile.checked
+
+    if (id == true){
+        location.href="english/index-en.html"
+    }else{
+        location.href="../index-es.html"
+    }
+}
+
+
+// codigo para menu hamburguesa - code of burger menu
+
+let cross = document.querySelector("#cross")
+let bars = document.querySelector("#bars")
+let menu = document.querySelector("#menu-mobile")
+
+cross.addEventListener("click", hiddenMenu)
+bars.addEventListener("click", showMenu)
+
+function hiddenMenu(){
+    menu.classList.toggle("hide-menu-mobile")
+}
+
+function showMenu(){
+    menu.classList.toggle("hide-menu-mobile")
+}
